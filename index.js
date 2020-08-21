38a72d63574cf018450033ec270009e417793899
Ross Rosenlof <rrosenlo@byu.edu>
2020-08-21 15:06:27 -0600

    starting to add palettes, plus site for viewing

---
+++ index.js
@@ -0,0 +1,20 @@
+import palettes from './palettes.js';
+
+function getRandom() {
+    return palettes[Math.floor(Math.random() * palettes.length)];
+}
+
+function get(name) {
+    if (name === undefined) return getRandom();
+    return palettes.find(pal => pal.name == name);
+  }
+  
+  function getAll() {
+    return palettes;
+  }
+  
+  function getNames() {
+    return palettes.map(p => p.name);
+  }
+
+export { getRandom, get, getAll, getNames };
---
+++ palettes-json.js
@@ -0,0 +1,282 @@
+let pals = [
+  {
+    name: "lawn",
+    harmony: "sc",
+    tags: "nature",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#4b6720', '#183617', '#c4cd47', '#2e717a', '#d7a0a4']
+  },
+  {
+    name: "prismatica",
+    harmony: "co",
+    tags: "nature",
+    background: "#000038",
+    stroke: "#000",
+    colors: ['#e37430', '#e3c39f', '#8ed2e6', '#7ef5db', '']
+  },
+  {
+    name: "timp",
+    harmony: "co",
+    tags: "nature",
+    background: "#BBCFC9",
+    stroke: "#000",
+    colors: ['#D6C19B', '#3c80bd', '#6B8B70', '', '']
+  },
+  {
+    name: "cabin",
+    harmony: "co",
+    tags: "nature",
+    background: "#2f2f2d",
+    stroke: "#D9D9D0",
+    colors: ['#5f4a49', '#a88671', '#879f85', '#336558', '']
+  },
+  {
+    name: "frijole",
+    harmony: "an",
+    tags: "food",
+    background: "#c3a66c",
+    stroke: "#000",
+    colors: ['#c97f1a', '#f5bf3c', '#ac703d', '#de3802', '']
+  },
+  {
+    name: "afghan",
+    harmony: "mo",
+    tags: "object",
+    background: "#efe5cc",
+    stroke: "#000",
+    colors: ['#d1bf25', '#666446', '#857d3f', '', '']
+  },
+  {
+    name: "bearpot",
+    harmony: "an",
+    tags: "object",
+    background: "#2d2e30",
+    stroke: "#000",
+    colors: ['#d39388', '#595350', '#d1c8cb', '#93746d', '#806f55']
+  },
+  {
+    name: "flathead",
+    harmony: "sc",
+    tags: "nature",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#3b7566', '#2d4242', '#263923', '#a3a194', '#df7c72']
+  },
+  {
+    name: "hotchicken",
+    harmony: "sc",
+    tags: "food",
+    background: "#000",
+    stroke: "#000",
+    colors: ['#3387da', '#FCFBEB', '#c24d0e', '#ffdc84', '']
+  },
+  {
+    name: "baja",
+    harmony: "mo",
+    tags: "food",
+    background: "#000",
+    stroke: "#A15AB3",
+    colors: ['#B7D7CA', '#95DCCF', '#67EDBE', '', '']
+  },
+  {
+    name: "shelf",
+    harmony: "co",
+    tags: "object",
+    background: "#f6f4f5",
+    stroke: "#000",
+    colors: ['#1c6029', '#663f35', '#ebdbc4', '#7f8893', '']
+  },
+  {
+    name: "shakespeare",
+    harmony: "tr",
+    tags: "artwork",
+    background: "#1F114A",
+    stroke: "#000",
+    colors: ['#D6E9F2', '#E4E0B8', '#be8b88', '', '']
+  },
+  {
+    name: "fence",
+    harmony: "co",
+    tags: "object",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#2791BB', '#248170', '#E1B473', '#b7a477', '']
+  },
+  {
+    name: "birthday",
+    harmony: "sq",
+    tags: "object",
+    background: "#726c49",
+    stroke: "#000",
+    colors: ['#e65131', '#306a74', '#90b33d', '#dcaf35', '']
+  },
+  {
+    name: "slam01",
+    harmony: "ds",
+    tags: "artwork",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#0c4a93', '#e3cb3e', '#dd7e45', '#7cbc74', '#F6B4A9']
+  },
+  {
+    name: "chirico",
+    harmony: "sc",
+    tags: "artwork",
+    background: "#D5C29C",
+    stroke: "#000",
+    colors: ['#234459', '#d29c1d', '#73663d', '#814110', '#372f1b']
+  },
+  {
+    name: "richter",
+    harmony: "an",
+    tags: "artwork",
+    background: "#230000",
+    stroke: "#000",
+    colors: ['#403A29', '#D6A451', '#8C2A14', '#BF705E', '']
+  },
+  {
+    name: "moa01",
+    harmony: "an",
+    tags: "artwork",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#c9993f', '#787b35', '#C24D04', '#787b35', '#666b5d']
+  },
+  {
+    name: "bluff",
+    harmony: "tr",
+    tags: "nature",
+    background: "#EEEAD0",
+    stroke: "#9d8361",
+    colors: ['#a35739', '#4E5C1B', '#66a2b4', '', '']
+  },
+  {
+    name: "crane",
+    harmony: "co",
+    tags: "object",
+    background: "#EBB591",
+    stroke: "#000",
+    colors: ['#ac5a1f', '#f4ede8', '#5a7478', '#BABECC', '']
+  },
+  {
+    name: "tidepool01",
+    harmony: "sc",
+    tags: "nature",
+    background: "#DDEEFF",
+    stroke: "#000",
+    colors: ['#a44e2b', '#8594a5', '#3a6929', '#d5c0a7', '']
+  },
+  {
+    name: "tidepool02",
+    harmony: "mo",
+    tags: "nature",
+    background: "#3b6a7a",
+    stroke: "#fff",
+    colors: ['#316c22', '#78995c', '#99b48b', '', '']
+  },
+  {
+    name: "castle",
+    harmony: "co",
+    tags: "object",
+    background: "#E6DADA",
+    stroke: "#000",
+    colors: ['#0f554a', '#72ceeb', '#c9615e', '#c62f2a', '']
+  },
+  {
+    name: "droid",
+    harmony: "an",
+    tags: "object",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#24AD70', '#B5A465', '#dfe0e4', '#c0dad7', '#abd3e4']
+  },
+  {
+    name: "carl",
+    harmony: "ds",
+    tags: "object",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#58abdb', '#b5b2ac', '#a99597', '#7C848A', '#1e2229']
+  },
+  {
+    name: "dorados",
+    harmony: "an",
+    tags: "food",
+    background: "#DDDDBB",
+    stroke: "#000",
+    colors: ['#9EAD2C', '#e9c32c', '#c18119', '#c0520e', '']
+  },
+  {
+    name: "monte01",
+    harmony: "sc",
+    tags: "artwork",
+    background: "#083c64",
+    stroke: "#000",
+    colors: ['#0298B3', '#EDD6B1', '#e69382', '#a92d19', '#BD7F23']
+  },
+  {
+    name: "ferry",
+    harmony: "an",
+    tags: "object",
+    background: "#CCEDDD",
+    stroke: "#C9BEBA",
+    colors: ['#767d69', '#34A357', '#187674', '#83b0ca', '']
+  },
+  {
+    name: "poppy01",
+    harmony: "mo",
+    tags: "nature",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#9a6031', '#e0b365', '#eee4c9', '', '']
+  },
+  {
+    name: "skylight",
+    harmony: "ds",
+    tags: "object",
+    background: "#FFFECE",
+    stroke: "#000",
+    colors: ['#fc0000', '#00ead9', '#00d62e', '#0054ff', '#ffa900']
+  },
+  {
+    name: "lowell",
+    harmony: "tr",
+    tags: "object",
+    background: "#000",
+    stroke: "#f1f3ff",
+    colors: ['#E6C440', '#E85168', '#3E76E6', '#84D9E9', '']
+  },
+  {
+    name: "af",
+    harmony: "mo",
+    tags: "object",
+    background: "#DDE1DF",
+    stroke: "#000",
+    colors: ['#f7d59c', '#d89556', '#6b5136', '#312214', '']
+  },
+  {
+    name: "jack",
+    harmony: "co",
+    tags: "object",
+    background: "#fff",
+    stroke: "#D4D4D4",
+    colors: ['#bd7628', '#0e1d23', '#0260a6', '', '']
+  },
+  {
+    name: "kernel",
+    harmony: "mo",
+    tags: "nature",
+    background: "#fff",
+    stroke: "#000",
+    colors: ['#e2d1b9', '#c3a163', '#d0971e', '', '']
+  },
+  {
+    name: "primary01",
+    harmony: "tr",
+    tags: "design",
+    background: "#000",
+    stroke: "#000",
+    colors: ['#30a6e6', '#ffff75', '#ff7169', '', '']
+  }
+]
