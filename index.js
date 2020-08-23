552fd1afa10e895275c79ac3b892c50910d3f777
ross <rosenlof.ross@gmail.com>
2020-08-22 19:00:46 -0600

    adding more functions

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // import palettes from './palettes.js';
 import palettes from './palettes-json.js'
 
+function getAll() {
+  return palettes;
+}
+
 function getRandom() {
   return palettes[Math.floor(Math.random() * palettes.length)];
 }
 
 function get(name) {
-  if (name === undefined) return getRandom();
-  return palettes.find(pal => pal.name == name);
-}
-
-function getAll() {
-  return palettes;
+  var pal = palettes.find(pal => pal.name == name);
+  return (pal ? pal : 'No palette with that name')
 }
 
 function getNames() {
@@ -19,21 +19,33 @@ function getNames() {
 }
 
 function getAllByHarmony(harmony) {
-  if (harmony === undefined) return getRandom();
   var pals = palettes.filter(pal => pal.harmony == harmony)
   return (pals.length == 0 ?  'No palettes with that harmony code' :  pals);
 }
 
 function getAllByTags(tag) {
-  if (tag === undefined) return getRandom();
   var pals = palettes.filter(pal => pal.tags == tag)
   return (pals.length == 0 ?  'No palettes with that tag' :  pals);
 }
 
 function getAllBySize(size) {
-  if (size === undefined) return getRandom();
   var pals = palettes.filter(pal => pal.colors.length == size);
   return (pals.length == 0 ?  'No palettes of that size' :  pals);
 }
 
-export { getRandom, get, getAll, getNames, getAllByHarmony, getAllByTags, getAllBySize };
+function getRandomByHarmony(harmony) {
+  var pals = getAllByHarmony(harmony)
+  return pals[Math.floor(Math.random() * pals.length)]
+}
+
+function getRandomByTag(tag) {
+  var pals = getAllByTag(tag)
+  return pals[Math.floor(Math.random() * pals.length)]
+}
+
+function getRandomBySize(size) {
+  var pals = getAllBySize(size)
+  return pals[Math.floor(Math.random() * pals.length)]
+}
+
+export { getRandom, get, getAll, getNames, getAllByHarmony, getAllByTags, getAllBySize, getRandomByHarmony, getRandomByTag, getRandomBySize };
